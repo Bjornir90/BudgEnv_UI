a9ff4f0f56e05aad4ced08748b12dbb54649a0b6
Bjornir90 <celestincollin@gmail.com>
2021-12-15 14:25:41 +0100

    Run prettier

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
-import servestatic from 'serve-static'
-import path from 'path'
-import dotenv from 'dotenv'
-import express from 'express'
+import servestatic from "serve-static";
+import path from "path";
+import dotenv from "dotenv";
+import express from "express";
 
 dotenv.config();
 
 const app = express();
 
-app.use(servestatic(path.join(path.resolve(), 'dist')));
+app.use(servestatic(path.join(path.resolve(), "dist")));
 
 app.listen(8080);
 
