3e7b35ecde5317bb1df723df9a2ddc3dbee14e3b
Bjornir90 <celestincollin@gmail.com>
2021-12-15 18:54:04 +0100

    Lint

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
-	<v-app>
-		<v-app-bar app class="primary">
-			<v-btn text to="/transactions">Transactions</v-btn>
+  <v-app>
+    <v-app-bar app class="primary">
+      <v-btn text to="/transactions">Transactions</v-btn>
 
-			<v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-			<v-btn text to="/">BudgEnv</v-btn>
+      <v-btn text to="/">BudgEnv</v-btn>
 
-			<v-spacer></v-spacer>
+      <v-spacer></v-spacer>
 
-			<v-btn text to="/login" v-if="!isLoggedIn"
-				><mdicon name="lock" />Login</v-btn
-			>
-		</v-app-bar>
+      <v-btn text to="/login" v-if="!isLoggedIn"
+        ><mdicon name="lock" />Login</v-btn
+      >
+    </v-app-bar>
 
-		<v-main>
-			<router-view />
-		</v-main>
-	</v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
@@ -25,19 +25,19 @@ import Vue from "vue";
 import { mapState } from "vuex";
 
 export default Vue.extend({
-	name: "App",
+  name: "App",
 
-	components: {},
+  components: {},
 
-	data: () => ({
-		//
-	}),
+  data: () => ({
+    //
+  }),
 
-	computed: {
-		isLoggedIn() {
-			return this.token !== "";
-		},
-		...mapState(["token"]),
-	},
+  computed: {
+    isLoggedIn() {
+      return this.token !== "";
+    },
+    ...mapState(["token"]),
+  },
 });
 </script>
--- src/views/Transactions.vue
+++ src/views/Transactions.vue
@@ -1,12 +1,8 @@
 <template>
-	<v-container>
-		<v-data-table
-			:headers="headers"
-			:items="transactions"
-			:items-per-page="15"
-		>
-		</v-data-table>
-	</v-container>
+  <v-container>
+    <v-data-table :headers="headers" :items="transactions" :items-per-page="15">
+    </v-data-table>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -15,61 +11,61 @@ import { mapState } from "vuex";
 import { Category, Transaction } from "../common";
 
 export default Vue.extend({
-	name: "Transactions",
+  name: "Transactions",
 
-	computed: {
-		...mapState(["categories", "token"]),
-	},
+  computed: {
+    ...mapState(["categories", "token"]),
+  },
 
-	data: () => {
-		return {
-			transactions: [] as Transaction[],
-			transactionsLoadFailed: false,
-			headers: [
-				{
-					text: "Payee",
-					value: "payee",
-				},
-				{
-					text: "Date",
-					value: "date",
-				},
-				{
-					text: "Note",
-					value: "memo",
-				},
-				{
-					text: "Amount",
-					value: "amount",
-				},
-			],
-		};
-	},
+  data: () => {
+    return {
+      transactions: [] as Transaction[],
+      transactionsLoadFailed: false,
+      headers: [
+        {
+          text: "Payee",
+          value: "payee",
+        },
+        {
+          text: "Date",
+          value: "date",
+        },
+        {
+          text: "Note",
+          value: "memo",
+        },
+        {
+          text: "Amount",
+          value: "amount",
+        },
+      ],
+    };
+  },
 
-	mounted: function () {
-		const currentDay = new Date();
-		let aMonthAgo = new Date();
-		aMonthAgo.setDate(currentDay.getDate() - 30);
+  mounted: function () {
+    const currentDay = new Date();
+    let aMonthAgo = new Date();
+    aMonthAgo.setDate(currentDay.getDate() - 30);
 
-		this.$http
-			.get(`${process.env.VUE_APP_API_URL}/transactions/range`, {
-				params: {
-					start_date: aMonthAgo.toISOString().split("T")[0],
-					end_date: currentDay.toISOString().split("T")[0],
-				},
-				headers: { Authorization: `Bearer ${this.token}` },
-			})
-			.then(
-				(response) => {
-					this.transactions = response.data;
-				},
-				(err) => {
-					this.transactionsLoadFailed = true;
-					console.log(err);
-				}
-			);
-	},
+    this.$http
+      .get(`${process.env.VUE_APP_API_URL}/transactions/range`, {
+        params: {
+          start_date: aMonthAgo.toISOString().split("T")[0],
+          end_date: currentDay.toISOString().split("T")[0],
+        },
+        headers: { Authorization: `Bearer ${this.token}` },
+      })
+      .then(
+        (response) => {
+          this.transactions = response.data;
+        },
+        (err) => {
+          this.transactionsLoadFailed = true;
+          console.log(err);
+        }
+      );
+  },
 
-	methods: {},
+  methods: {},
 });
 </script>
